7250ca2f70fd9866ff8dcf9c3745e459cfc84e23
Ulf Lilleengen <lulf@redhat.com>
2022-05-10 10:33:19 +0200

    Add scripts

---
+++ update.sh
@@ -0,0 +1,1 @@
+while true; do drgdfu -v upload ble-gatt --device E2:9A:A8:1C:CB:0A cloud --http https://http.sandbox.drogue.cloud --application eclipse-iot-day --device microbit --password hey-rodney; done
