565eecfcd9d58b25e9fc9e4cc0ef3b41aa266f3b
Ulf Lilleengen <lulf@redhat.com>
2022-05-10 14:36:36 +0200

    Update to E

--- firmware/application/src/main.rs
+++ firmware/application/src/main.rs
@@ -116,7 +116,7 @@ async fn main(s: Spawner, p: Peripherals) {
     display.set_brightness(Brightness::MAX);
     loop {
         let _ = display
-            .display('A'.to_frame(), Duration::from_secs(1))
+            .display('E'.to_frame(), Duration::from_secs(1))
             .await;
         Timer::after(Duration::from_secs(1)).await;
     }
