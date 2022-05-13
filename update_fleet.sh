cbfa7b7fecc6ef32e8a3b004509ac196e7ab1df9
Ulf Lilleengen <lulf@redhat.com>
2022-05-13 10:03:54 +0200

    Add toolchain file

---
+++ update_fleet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+INITIAL_REV=${1:-"0000"}
+set -x
+
+for i in $(seq 1 10)
+do
+    echo "Running simulator for device-sim-${i}"
+    drgdfu upload simulated --version ${INITIAL_REV} cloud --http https://http.sandbox.drogue.cloud --application eclipse-iot-day --device device-sim-${i} --password 1234 &
+    pids[${i}]=$!
+    sleep 2
+done
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
