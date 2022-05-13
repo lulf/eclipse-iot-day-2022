145024b9fd4e09e3885a51640da6fa62bff24d09
Ulf Lilleengen <lulf@redhat.com>
2022-05-13 10:57:50 +0200

    Use stm32 app

--- simulated/src/main.rs
+++ simulated/src/main.rs
@@ -1,19 +1,14 @@
 #![no_std]
 #![no_main]
 
-extern crate libc;
+use defmt_rtt as _;
+use panic_probe as _;
 
-#[no_mangle]
-pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // Since we are passing a C string the final null character is mandatory.
-    const HELLO: &'static str = "Hello, world!\n\0";
-    unsafe {
-        libc::printf(HELLO.as_ptr() as *const _);
-    }
-    0
-}
+#[allow(unused_imports)]
+use stm32l4::stm32l4x5 as pac;
 
-#[panic_handler]
-fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::println!("Hello, World!");
     loop {}
 }
